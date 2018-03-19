7f89902b96fccf195e4a2e43acf888c682d07877
Evgeny Knyazev <knyazev.evgeny@gmail.com>
2018-03-19 18:46:58 +0400

    Initial Commit

---
+++ FirstAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstAlert
+//
+//  Created by Evgeny Knyazev on 19.03.2018.
+//  Copyright © 2018 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstAlert
+//
+//  Created by Evgeny Knyazev on 19.03.2018.
+//  Copyright © 2018 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
